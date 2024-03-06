76eb74be1df9ed4324aa22006529ec0b86f35c54
notCroptu <martinsmariana1972@gmail.com>
2024-03-06 17:20:32 +0000

    ArrayMult Ex3

---
+++ ArrayMult/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ArrayMult
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float [,] FinalArray = new float [2, 1];
+            FinalArray[0, 0] = float.Parse(args[0]) * float.Parse(args[4]) + float.Parse(args[1]) * float.Parse(args[5]);
+            FinalArray[1, 0] = float.Parse(args[2]) * float.Parse(args[4]) + float.Parse(args[3]) * float.Parse(args[5]);
+
+            Console.WriteLine(FinalArray[0, 0]);
+            Console.WriteLine(FinalArray[1, 0]);
+        }
+    }
+}

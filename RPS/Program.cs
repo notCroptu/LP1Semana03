761a9ae2c3094e60f92c23e9ca2c348e1382c1b9
notCroptu <martinsmariana1972@gmail.com>
2024-03-06 23:31:13 +0000

    RPS added proper return

---
+++ RPS/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace RPS
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            int result = RockPaperScissors(args[0], args[1]);
+            switch (result)
+            {
+                case 0:
+                    Console.WriteLine("It's a draw!");
+                    break;
+                case 1:
+                    Console.WriteLine("Player 1 wins!");
+                    break;
+                case 2:
+                    Console.WriteLine("Player 2 wins!");
+                    break;
+            }
+        }
+
+        private static int RockPaperScissors(string player1, string player2)
+        {
+            int result = -1;
+
+            if (player1 == player2)
+            {
+                result = 0; // Draw
+            }
+            if (((player1 == "Rock") && (player2 == "Scissors")) ||
+                ((player1 == "Scissors") && (player2 == "Paper")) ||
+                ((player1 == "Paper") && (player2 == "Rock")))
+            {
+                result = 1; // Player 1 wins
+            }
+            else
+            {
+                result = 2; // Player 2 wins
+            }
+            return result;
+        }
+    }
+}

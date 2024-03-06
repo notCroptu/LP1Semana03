0fd34407a6f19c8b51fb2921a9fe77364a127586
notCroptu <martinsmariana1972@gmail.com>
2024-03-06 23:08:48 +0000

    PowTwoUntil Ex5

---
+++ PowTwoUntil/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace PowTwoUntil
+{
+    class Program
+    {
+        private static void PowersOf2Until5()
+        {
+            for (int i = 1; i <= (1 << 5); i = i << 1)
+            {
+                Console.WriteLine(i);
+            }
+        }
+        static void Main(string[] args)
+        {
+            PowersOf2Until5();
+            PowersOf2Until5();
+        }
+    }
+}

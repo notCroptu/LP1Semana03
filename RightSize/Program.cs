633b4517fa1cfa3f5a3de6a6804ec78c50453d0b
notCroptu <martinsmariana1972@gmail.com>
2024-03-06 11:33:43 +0000

    RightSize Ex1

---
+++ RightSize/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace RightSize
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (string s in args)
+            {
+                if (s.Length >= 3 && s.Length < 6)
+                {
+                    Console.WriteLine(s);
+                }
+                else if (s.Length >= 6)
+                {
+                    break;
+                }
+            }
+
+        }
+    }
+}

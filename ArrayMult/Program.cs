c6d1ecda13655aa91e947d26136216de585305b7
notCroptu <martinsmariana1972@gmail.com>
2024-03-07 16:08:50 +0000

    Fixed ArrayMult Ex4

--- ArrayMult/Program.cs
+++ ArrayMult/Program.cs
@@ -6,12 +6,31 @@ namespace ArrayMult
     {
         static void Main(string[] args)
         {
-            float [,] FinalArray = new float [2, 1];
-            FinalArray[0, 0] = float.Parse(args[0]) * float.Parse(args[4]) + float.Parse(args[1]) * float.Parse(args[5]);
-            FinalArray[1, 0] = float.Parse(args[2]) * float.Parse(args[4]) + float.Parse(args[3]) * float.Parse(args[5]);
 
-            Console.WriteLine(FinalArray[0, 0]);
-            Console.WriteLine(FinalArray[1, 0]);
+            float [][] FirstArray = new float [2][];
+            FirstArray[0] = new float [2] {float.Parse(args[0]), float.Parse(args[1])};
+            FirstArray[1] = new float [2] {float.Parse(args[2]), float.Parse(args[3])};
+
+            float [][] SecondArray = new float [2][];
+            SecondArray[0] = new float [1] {float.Parse(args[4])};
+            SecondArray[1] = new float [1] {float.Parse(args[5])};
+
+            float [][] FinalArray = new float [2][];
+            FinalArray[0] = new float [1] {0};
+            FinalArray[1] = new float [1] {0};
+
+            for (int i = 0;  i < FinalArray.Length; i += 1)
+            {
+                for (int e = 0; e < FinalArray[i].Length; e += 1)
+                {
+                    FinalArray[i] = new float [1] {FirstArray[i][0] * SecondArray[0][0] + FirstArray[i][1] * SecondArray[1][0]};
+                }
+            }
+            //FinalArray[0, 0] = float.Parse(args[0]) * float.Parse(args[4]) + float.Parse(args[1]) * float.Parse(args[5]);
+            //FinalArray[1, 0] = float.Parse(args[2]) * float.Parse(args[4]) + float.Parse(args[3]) * float.Parse(args[5]);
+
+            Console.WriteLine(FinalArray[0][0]);
+            Console.WriteLine(FinalArray[1][0]);
         }
     }
 }
